2a8b5c285f0b36c6b8bbc5f04c2d319bbc9d90fd
xa zhu xj <xazhuxj@qq.com>
2020-05-19 21:19:46 +0800

    重修修改了程序架构，做了一定的简化

--- ProjApp/StartupWindow.xaml.cs
+++ ProjApp/StartupWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProjApp.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,11 +20,14 @@ namespace ProjApp
     /// </summary>
     public partial class StartupWindow : Window
     {
+        private ProjWinVM vm;
+
         public StartupWindow()
         {
             InitializeComponent();
+            this.DataContext = vm = new ProjWinVM();
         }
-        private void OnShowDialog<T>() where T:Window, new()
+        private void OnShowDialog<T>() where T : Window, new()
         {
             T frm = new T();
             frm.Owner = this;
@@ -32,19 +36,39 @@ namespace ProjApp
         }
 
 
-        private void OnSinglePointProj_Click(object sender, RoutedEventArgs e)
-        {
-            OnShowDialog<SinglePointProjWin>();            
-        }
-
-        private void OnMultiPointProj_Click(object sender, RoutedEventArgs e)
-        {          
-            OnShowDialog<MultiPointProjWin>();
-        }
-
         private void OnAzimuth_Click(object sender, RoutedEventArgs e)
         {
             OnShowDialog<AzimuthWin>();
+        }
+
+        private void OnReadDataFile(object sender, RoutedEventArgs e)
+        {
+            vm.ReadDataFile();
+        }
+
+        private void OnWriteDataFile(object sender, RoutedEventArgs e)
+        {
+            vm.WriteDataFile();
+        }
+
+        private void OnClearBL(object sender, RoutedEventArgs e)
+        {
+            vm.ClearBL();
+        }
+
+        private void OnClearXY(object sender, RoutedEventArgs e)
+        {
+            vm.ClearXY();
+        }
+
+        private void OnBLtoXYProj(object sender, RoutedEventArgs e)
+        {
+            vm.BLtoXY();
+        }
+
+        private void OnXYtoBLProj(object sender, RoutedEventArgs e)
+        {
+            vm.XYtoBL();
         }
     }
 }

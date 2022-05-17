0f105ec357c8a5f37294362ca022003411f5cc81
zhuxj <xazhuxj@qq.com>
2022-05-17 13:14:32 +0800

    改版为 .net 6

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ProjApp
+
+使用 CommunityToolkit.Mvvm (Microsoft.Toolkit) 架构完成界面的绑定（binding）
---
+++ SurMath/Point.cs
@@ -0,0 +1,47 @@
+﻿namespace ZXY
+{
+    public class Point
+    {
+        private string name;
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        private string code;
+        public string Code
+        {
+            get => code;
+            set => code = value;
+        }
+
+        private double x;
+        public double X
+        {
+            get => x;
+            set => x = value;
+        }
+
+        private double y;
+        public double Y
+        {
+            get => y;
+            set => y = value;
+        }
+
+        private double z;
+        public double Z
+        {
+            get => z;
+            set => z = value;
+        }
+
+
+        public override string ToString()
+        {
+            return $"{Name}, {Code}, {X}, {Y}, {Z}";
+        }
+
+    }
+}
